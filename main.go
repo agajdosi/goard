14e77dcb31d50ad87713425aea097093e8385cd9
agajdosi <agajdosi@redhat.com>
2018-10-03 14:40:11 +0200

    getFile to accept local paths and remote URL

--- main.go
+++ main.go
@@ -4,33 +4,55 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 func main() {
 	result, _ := checkFiles("test/eap71-basic-s2i.json", "https://raw.githubusercontent.com/minishift/minishift/master/addons/xpaas/v3.10/xpaas-templates/eap71-basic-s2i.json")
 	fmt.Println(result)
 
+	result, _ = checkFiles("https://raw.githubusercontent.com/minishift/minishift/master/addons/xpaas/v3.10/xpaas-templates/eap71-basic-s2i.json", "test/eap71-basic-s2i.json")
+	fmt.Println(result)
+
 	result, _ = checkFiles("test/eap71-basic-s2i-wrong.json", "https://raw.githubusercontent.com/minishift/minishift/master/addons/xpaas/v3.10/xpaas-templates/eap71-basic-s2i.json")
 	fmt.Println(result)
 }
 
-func checkFiles(path, url string) (bool, error) {
-	remoteFile, err := getFile(url)
+func checkFiles(pathOne, pathTwo string) (bool, error) {
+	first, err := getFile(pathOne)
 	if err != nil {
 		return false, err
 	}
 
-	localFile, err := readFile(path)
+	second, err := getFile(pathTwo)
 	if err != nil {
 		return false, err
 	}
 
-	result := areSame(localFile, remoteFile)
-
-	return result, nil
+	equals := first == second
+	return equals, nil
 }
 
-func getFile(url string) (string, error) {
+func getFile(path string) (string, error) {
+	parsedURL, err := url.Parse(path)
+	if err != nil {
+		return "", err
+	}
+
+	var content string
+	switch parsedURL.Scheme {
+	case "http", "https":
+		content, err = downloadFile(path)
+	case "", "file":
+		content, err = readFile(path)
+	default:
+		err = fmt.Errorf("unable to ")
+	}
+
+	return content, err
+}
+
+func downloadFile(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", err
@@ -56,12 +78,4 @@ func readFile(path string) (string, error) {
 	}
 
 	return string(remoteFile), nil
-}
-
-func areSame(stringOne, stringTwo string) bool {
-	if stringOne != stringTwo {
-		return false
-	}
-
-	return true
 }
