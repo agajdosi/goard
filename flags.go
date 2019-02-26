6d0cd12ee07d24c1c7ae09659ff8200864006dff
agajdosi <agajdosi@redhat.com>
2019-02-26 17:35:59 +0100

    Adding proper example files with extensive explanatory comments

--- flags.go
+++ flags.go
@@ -8,7 +8,7 @@ var (
 )
 
 func getFlags() {
-	flag.StringVar(&configPath, "config-path", "test/config.yaml", "Path to the config")
+	flag.StringVar(&configPath, "config-path", "goard-config.yaml", "Path to the config")
 	flag.StringVar(&workDir, "work-dir", "", "Change working directory of the command. Use current directory if left empty.")
 	flag.Parse()
 }
